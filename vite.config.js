eb6ab2c1d5e5e362f15ce18e8043401df4108115
kittyli-spec <459792256@qq.com>
2024-12-25 21:57:01 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    hmr: {
-      overlay: false
-    },
-    port: 5173,
-    open: true
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
-  }
-}) 
+})
